9a56fccf5c95ce88eb657e92a66918d450a2d1ee
stoc9980 <122923243+stoc9980@users.noreply.github.com>
2023-01-17 12:58:14 -0600

    Update streamlit_app.py
    Hopefully now fixed code for Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('Omega 3 and Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach and Rocket Smoothie')
 
-streamlit.text('Hard-Boiled Free-Range Egg)
+streamlit.text('Hard-Boiled Free-Range Egg')
